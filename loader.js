bc38da3ebd74112d49f84e0c90541b156fd23da9
NamNam2727 <azkank@gmail.com>
2026-05-24 03:05:53 +0900

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,26 +1,18 @@
-// =========================================================
-// loader.js
-// =========================================================
-
 (async () => {
 
 "use strict";
-
-/* =========================================================
-   SCRIPT LIST
-========================================================= */
 
 const scripts = [
 
     "https://namnam2727.github.io/MMO/movement.js",
 
     "https://namnam2727.github.io/MMO/main.js"
-
 ];
 
-/* =========================================================
-   LOAD SCRIPT
-========================================================= */
+for(const src of scripts){
+
+    await loadScript(src);
+}
 
 function loadScript(src){
 
@@ -31,65 +23,12 @@ function loadScript(src){
 
         script.src = src;
 
-        // 読み込み順維持
-        script.async = false;
+        script.onload = resolve;
 
-        script.onload = () => {
+        script.onerror = reject;
 
-            console.log(
-                "[Loader] Loaded:",
-                src
-            );
-
-            resolve();
-        };
-
-        script.onerror = () => {
-
-            console.error(
-                "[Loader] Failed:",
-                src
-            );
-
-            reject(src);
-        };
-
-        document.head.appendChild(script);
+        document.body.appendChild(script);
     });
 }
-
-/* =========================================================
-   LOAD ALL
-========================================================= */
-
-async function loadAll(){
-
-    for(const src of scripts){
-
-        await loadScript(src);
-    }
-
-    console.log(
-        "[Loader] All scripts loaded"
-    );
-
-    // 少し待ってから開始
-    setTimeout(() => {
-
-        if(typeof window.startGame === "function"){
-
-            window.startGame();
-        }
-        else{
-
-            console.warn(
-                "[Loader] startGame not found"
-            );
-        }
-
-    }, 0);
-}
-
-loadAll();
 
 })();
