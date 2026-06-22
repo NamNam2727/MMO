1c48310978382d07f1e740a8aee9ce33a0212913
NamNam2727 <azkank@gmail.com>
2026-06-22 22:41:37 +0900

    Add files via upload

--- maps/town/npc1.js
+++ maps/town/npc1.js
@@ -3,34 +3,30 @@
 // はじまりの街に配置するNPCのデータを定義する
 // =========================================================
 
-// ENEMY_DB とは別に、友好的なNPC用のデータベースを用意します
 window.NPC_DB = window.NPC_DB || {};
 
 window.NPC_DB['town_merchant_1'] = {
     name: '商人',
-    type: 'npc',          // ★重要: npcタイプは攻撃できないように制御します
-    color: '#ffaa00',     // 黄色っぽい色
+    type: 'npc',          
+    color: '#ffaa00',     
     radius: 15,
-    
-    // 商人の画像URLがあれば指定
     imageUrl: '', 
     
-    // 話しかけた（タップした）時のイベント処理
     interact: function(npc, player) {
         if (typeof window.addLog === 'function') {
             window.addLog(`<span class='color-sys'>商人: 「いらっしゃい！何か買っていくかい？」</span>`, 'sys');
         }
-        
-        // ★次回作成するショップUIを開く処理を呼び出す
         if (typeof window.openShopWindow === 'function') {
             window.openShopWindow(npc);
         }
     },
 
-    // この商人が販売しているアイテムのIDリスト
+    // ★修正: オブジェクト形式でレアリティ(rarity)を指定できるようにしました
     shopItems: [
-        'potion_small',
+        'potion_small',    // 文字列だけの指定も今まで通り可能
         'sword_wood',
-        'armor_leather'
+        { id: 'sword_fire', rarity: 'Uncommon' }, // レアリティ指定販売
+        { id: 'chip_enemy_single_str', rarity: 'Rare' },
+        { id: 'chip_enemy_single_int', rarity: 'Epic' }
     ]
 };
